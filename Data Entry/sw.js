c8cebb1a0a47242ddfffa552788b701b5f28d629
Michael Garrison <michael@triumph.tech>
2022-09-08 22:35:50 -0700

    Another attempt at a service worker

--- Data Entry/sw.js
+++ Data Entry/sw.js
@@ -1,28 +1,30 @@
-let cache_name = "CocoNutsScouting";
-
-self.addEventListener("fetch", event => {
-    if (event.request.url === "https://mikejed.github.io/") {
-        event.respondWith(
-            fetch(event.request).catch(err =>
-                self.cache.open(cache_name).then(cache => cache.match("/offline.html"))
-            )
-        );
-    } else {
-        event.respondWith(
-            fetch(event.request).catch(err =>
-                caches.match(event.request).then(response => response)
-            )
-        );
-    }
-});
-
-self.addEventListener("fetch", event => {
-    console.log("You fetched " + event.url);
-});
-
+const cacheName = "CocoNutsScouting";
 const ASSETS = [
+    "/CocoNuts-Scouting/Data%20Entry/scoring-sheet.html",
     "/CocoNuts-Scouting/Data%20Entry/qrcode.min.js",
     "/CocoNuts-Scouting/Data%20Entry/bootstrap.min.css",
-    "/CocoNuts-Scouting/Data%20Entry/scoring-sheet.html",
     "/CocoNuts-Scouting/Data%20Entry/jquery-3.6.1.min.js"
 ];
+
+// When the service worker is installing, open the cache and add the precache resources to it
+self.addEventListener('install', (event) => {
+    console.log('Service worker install event!');
+    event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+  });
+  
+  self.addEventListener('activate', (event) => {
+    console.log('Service worker activate event!');
+  });
+  
+  // When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
+  self.addEventListener('fetch', (event) => {
+    console.log('Fetch intercepted for:', event.request.url);
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request);
+      }),
+    );
+  });
