cca4e459f314161475387c47401187965045e5e4
Michael Garrison <michael@triumph.tech>
2022-09-17 07:15:25 -0700

    Service Worker changes for offline

--- Assets/sw.js
+++ Assets/sw.js
@@ -7,32 +7,24 @@ const precacheResources = [
     "/CocoNuts-Scouting/Assets/jquery-3.6.1.min.js"
 ];
 
-// install event handler (note async operation)
-// opens named cache, pre-caches identified resources above
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil((async () => {
     const cache = await caches.open(cacheName);
-    cache.addAll(precacheResources);
+    await cache.addAll(precacheResources);
   })());
 });
 
-self.addEventListener('activate', event => {
-  console.log('Service worker activate event!');
-});
+self.addEventListener('fetch', (event) => {
+  event.respondWith((async () => {
+    // try the cache first
+    const r = await caches.match(event.request);
+    if (r) { return r; }
 
-// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
-self.addEventListener('fetch', event => {
-  event.respondWith(async () => {
+    // cache the new resource and return it
+    const response = await fetch(e.request);
     const cache = await caches.open(cacheName);
 
-    // Try the cache first.
-    const cachedResponse = await cache.match(event.request);
-    if (cachedResponse !== undefined) {
-      // Cache hit, let's send the cached resource.
-      return cachedResponse;
-    } else {
-      // Nothing in cache, let's go to the network.
-      return fetch(event.request);
-    }
-  }
-)});
+    cache.put(e.request, response.clone());
+    return response;
+  })());
+});
