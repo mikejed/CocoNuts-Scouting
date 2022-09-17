b9be0639949aa1ffda4423b0081145c1efc898c1
Michael Garrison <michael@triumph.tech>
2022-09-16 23:31:12 -0700

    Try another version of making available offline

--- Assets/sw.js
+++ Assets/sw.js
@@ -1,30 +1,38 @@
 const cacheName = "CocoNutsScouting";
 const precacheResources = [
+    "/",
     "/CocoNuts-Scouting/index.html",
     "/CocoNuts-Scouting/Assets/qrcode.min.js",
     "/CocoNuts-Scouting/Assets/bootstrap.min.css",
     "/CocoNuts-Scouting/Assets/jquery-3.6.1.min.js"
 ];
 
-// When the service worker is installing, open the cache and add the precache resources to it
-self.addEventListener('install', (event) => {
-    console.log('Service worker install event!');
-    event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
-  });
-  
-  self.addEventListener('activate', (event) => {
-    console.log('Service worker activate event!');
-  });
-  
-  // When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
-  self.addEventListener('fetch', (event) => {
-    console.log('Fetch intercepted for:', event.request.url);
-    event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-        return fetch(event.request);
-      }),
-    );
-  });
+// install event handler (note async operation)
+// opens named cache, pre-caches identified resources above
+self.addEventListener('install', event => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    cache.addAll(precacheResources);
+  })());
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service worker activate event!');
+});
+
+// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
+self.addEventListener('fetch', event => {
+  event.respondWith((async () => {
+    const cache = await caches.open(cacheName);
+
+    // Try the cache first.
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse !== undefined) {
+      // Cache hit, let's send the cached resource.
+      return cachedResponse;
+    } else {
+      // Nothing in cache, let's go to the network.
+      return fetch(event.request);
+    }
+  }
+});
