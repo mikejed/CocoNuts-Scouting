03b6a0cb360a1b767472eef27dfcefbe43950bda
Michael Garrison <michael@triumph.tech>
2022-09-18 17:36:43 -0700

    Remove assets subdirectory to fix offline functionality

--- sw.js
+++ sw.js
@@ -2,10 +2,10 @@ const cacheName = "CocoNutsScouting";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
-    "/CocoNuts-Scouting/Assets/qrcode.min.js",
-    "/CocoNuts-Scouting/Assets/bootstrap.min.css",
-    "/CocoNuts-Scouting/Assets/bootstrap.min.css.map",
-    "/CocoNuts-Scouting/Assets/jquery-3.6.1.min.js"
+    "/CocoNuts-Scouting/qrcode.min.js",
+    "/CocoNuts-Scouting/bootstrap.min.css",
+    "/CocoNuts-Scouting/bootstrap.min.css.map",
+    "/CocoNuts-Scouting/jquery-3.6.1.min.js"
 ];
 
 self.addEventListener('install', (event) => {
