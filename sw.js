329c4eb7a0808432a3f82e0585e529a7c4c36aff
Connor Norris <99221420+ConnorJNorris@users.noreply.github.com>
2023-02-18 15:39:22 -0700

    Changed team number options

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2023-02-11_3";
+const cacheName = "CocoNutsScouting_2023-02-11_4";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
