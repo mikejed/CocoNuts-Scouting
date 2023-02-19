9ad0f737721bb11685f010311a5371bd572d86dc
Connor Norris <99221420+ConnorJNorris@users.noreply.github.com>
2023-02-18 17:05:10 -0700

    added a comment

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2023-02-11_5";
+const cacheName = "CocoNutsScouting_2023-02-11_6";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
