2718fe0b8f5386afb2cfc097605b59c5f9145c83
Connor Norris <99221420+ConnorJNorris@users.noreply.github.com>
2023-03-04 09:47:27 -0700

    changed cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2023-03-4_1";
+const cacheName = "CocoNutsScouting_2023-03-4_2";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
