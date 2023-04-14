9880e321a7bd3f8ee5997a435082882f78b785eb
Connor Norris <99221420+ConnorJNorris@users.noreply.github.com>
2023-04-14 11:36:06 -0700

    Added more teams to list

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2023-04-14_1";
+const cacheName = "CocoNutsScouting_2023-04-14_2";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
