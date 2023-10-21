68e39a09d0586117df39bc8a72f48b112e2b04e8
Connor Norris <99221420+ConnorJNorris@users.noreply.github.com>
2023-10-20 22:17:24 -0700

    Updated team list for AZ champs

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2023-04-20_1";
+const cacheName = "CocoNutsScouting_2023-10-20_1";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
